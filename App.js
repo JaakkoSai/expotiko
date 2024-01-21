7a2384d5f2530f439fd89d7b0cacd9b743772a60
JaakkoSai <91462991+JaakkoSai@users.noreply.github.com>
2024-01-21 15:57:45 +0200

    first changes

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { Alert } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [text, setText] = useState("");
+
+  const buttonPressed = () => {
+    Alert.alert("You typed " + text);
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
+      <TextInput
+        style={{ width: 200, borderColor: "gray", borderWidth: 1 }}
+        onChangeText={(text) => setText(text)}
+        value={text}
+      />
+      <Button onPress={buttonPressed} title="press me" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
